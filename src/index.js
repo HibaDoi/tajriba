ded3dd22d65e7d9529248cc565fbd1edf3fc82c6
hiba doi <doihiba@iav.ac.ma>
2023-12-02 22:27:42 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log('Initializing the map...');
+window.initMap = () => {
+    console.log('Initializing thehhhhhhhhhhhhhhhhhhhhhhhh map...');
+    const map =new google.maps.Map(document.getElementById('map'),
+    {center :{lat:40,lng:-100},
+    zoom:5,
+});
+}
+
+
